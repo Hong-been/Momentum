9228f9edb03765965d321c805fa5ef35cfcf6225
HongBeen <redbean-2@naver.com>
2021-07-12 00:27:43 +0900

    HTML 골격 제작으로 전체 클래스, 아이디 수정
    - top(날씨)
    - center
      - center-top(빈 공간)
      - center-mid(시간, 유저 인사말)
      - center-bottom(투두리스트 입력 및 출력)
    - bottom(명언)

--- js/weather.js
+++ js/weather.js
@@ -6,17 +6,17 @@ function onGeoOk(position){
     
     const url=`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${API_KEY}&units=metric`;
     fetch(url).then(response => response.json()).then(data=>{
-        const weather=document.querySelector('div#weather span:first-child');
-        const city=document.querySelector('div#weather span:nth-child(2)');
-        const temp=document.querySelector('div#weather span:nth-child(3)');
-        const feelLike=document.querySelector('div#weather span:nth-child(4)');
-        const humidity=document.querySelector('div#weather span:last-child');
+        const weather=document.querySelector('div#weather div:first-child');
+        const city=document.querySelector('div#weather div:nth-child(2)');
+        const temp=document.querySelector('div#weather div:nth-child(3)');
+        const feelLike=document.querySelector('div#weather div:nth-child(4)');
+        const humidity=document.querySelector('div#weather div:last-child');
 
         weather.innerText=`${data.weather[0].main}`;
-        city.innerText=`in ${data.name}\n`;
-        temp.innerText=`Temp ${Math.round(data.main.temp)}℃\n`;
-        feelLike.innerText=`Feels like ${Math.round(data.main.feels_like)}℃\n`;
-        humidity.innerText=`💦${data.main.humidity}%`;
+        city.innerText=`in ${data.name}`;
+        temp.innerText=`Temp ${Math.round(data.main.temp)}℃`;
+        feelLike.innerText=`Feels like ${Math.round(data.main.feels_like)}℃`;
+        humidity.innerText=`${data.main.humidity}% water vapor in air`;
 
         console.log(data.name,data.weather[0].main,
             data.main.temp, data.main.feels_like, data.main.humidity);
